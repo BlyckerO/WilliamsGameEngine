fd9836fee5c7e73e501bc43218b037495cd90347
Olin Blycker <blyckero@rsd407.org>
2022-04-26 11:01:06 -0700

    asdf

---
+++ Base project/MyGame/MyGame/Background.cpp
@@ -0,0 +1,41 @@
+#include "Meteor.h"  
+#include "GameScene.h" 
+#include "Explosion.h" 
+#include "Score.h"
+const float SPEED = 0.5f;
+
+Background::Background(sf::Vector2f pos)
+{
+	sprite_.setTexture(GAME.getTexture("Resources/background.png"));
+	sprite_.setPosition(pos);
+	assignTag("background");
+	
+}
+
+void Background::draw()
+{
+	GAME.getRenderWindow().draw(sprite_);
+
+}
+
+void Backgound::update(sf::Time& elapsed)
+{
+	int msElapsed = elapsed.asMilliseconds();
+	sf::Vector2f pos = sprite_.getPosition();
+
+	if (pos.x < sprite_.getGlobalBounds().width * -1)
+	{
+		GameScene& scene = (GameScene&)GAME.getCurrentScene();
+		scene.decreaseLives();
+		makeDead();
+
+	}
+	else
+	{
+		sprite_.setPosition(sf::Vector2f(pos.x - SPEED * msElapsed, pos.y));
+	}
+}
+sf::FloatRect Meteor::getCollisionRect()
+{
+	return sprite_.getGlobalBounds();
+}
---
+++ Base project/MyGame/MyGame/Background.h
@@ -0,0 +1,22 @@
+
+#pragma once
+#include "Engine/GameEngine.h" 
+
+class Background : public GameObject
+{
+public:
+	//Creates Meteor 
+	Background(sf::Vector2f pos);
+
+	// Functions overridn from GameObject 
+	void draw();
+	void update(sf::Time& elapsed);
+
+	
+	
+private:
+	sf::Sprite sprite_;
+
+};
+
+typedef std::shared_ptr<Background> MeteorPtr;
